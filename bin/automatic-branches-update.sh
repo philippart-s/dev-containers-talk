092f37bf1fb696f09ead0f054b3b6c9b15f3e65c
philippart-s <wildagsx@gmail.com>
2026-01-05 15:05:10 +0000

    clearn: 🗑️ Remove useless files

--- bin/automatic-branches-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(seq -w 0 11); do
-  git switch step-$i
-  git restore --source=main README.md
-  git add README.md
-  git commit -m "Sync fichier depuis main"
-  git push
-done
